e1c98c1ff9c7a52ee4634d4dd66413c3b8eafaab
kellywong911 <48376405+kellywong911@users.noreply.github.com>
2020-11-30 23:42:52 +1300

    Add files via upload

---
+++ FirstAML/Model/Order.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstAMLCore.Model
+{
+    public class Order
+    {
+        //set auto gen ID in database
+       public int orderNumber { get; set; }
+       public string Shipping { get; set; }
+
+        public List<Parcel> ParcelItem = new List<Parcel>();
+
+        //use async task 
+       public void AddParcel(Parcel parcel)
+        {
+            var item = ParcelItem.Any(x => x.ParcelNumber == parcel.ParcelNumber);
+            if (item)
+            {
+                throw new Exception($"ParcelNumber {parcel.ParcelNumber} is already exist.");
+            }
+            ParcelItem.Add(parcel);
+        }
+
+       
+       public void RemoveParcel(Parcel parcel)
+        {
+            var item = ParcelItem.Find(x => x.ParcelNumber == parcel.ParcelNumber);
+            if (item != null)
+            {
+                ParcelItem.Remove(parcel);
+            }
+            else 
+                throw new Exception($"{parcel.ParcelNumber} does not exists");
+        }
+
+        public double CalculateParcel()
+        {
+            var sum = 0.00;
+            foreach (var item in ParcelItem)
+            {
+                sum += item.DeliveryCost;
+            }
+            //use Constant or enum
+            if (Shipping == "Speedy")
+            {
+                sum = sum * 2;
+            }
+            return sum;
+        }
+
+        //sorry it's so messy, not working T^T
+        public double ApplyDiscount()
+        {
+            //order
+            var smallParcel = (int)ParcelItem.Select(x => x.Size == "S").Count()/4;
+            var mediumParcel = (int)ParcelItem.Select(x => x.Size == "M").Count()/3;
+            var mixParcel = (int)ParcelItem.Count/5;
+
+            foreach (var item in ParcelItem)
+            {
+                var sItem = 1;
+                var mItem = 1;
+                var lItem = 1;
+                var xlItem = 1;
+
+                //do similar for the rest
+                if (smallParcel > 0)
+                {
+                    if (item.Size == "S")
+                    {
+                        if (sItem == 4)
+                        {
+                            if (item.IsUsedInDiscount == false)
+                            {
+                                item.IsUsedInDiscount = true;
+                                //should make a field for extra saying its discounted Item?
+                                //and minus
+                                item.DeliveryCost = 0;
+                            }
+                            sItem = 1;
+                        }
+                        else 
+                            sItem++;
+                    }
+                }
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ FirstAML/Model/Parcel.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstAMLCore.Model
+{
+    //use constant Size small medium
+    public class Parcel
+    {
+        //get Guid Auto Gen
+        public int OrderNumber { get; set; }
+        public int ParcelNumber { get; set; }
+        public double MaxDimension { get; set; }
+        public double Weight { get; set; }
+        public double DeliveryCost { get; set; }
+        //should set as a string (small, medium, etc if occurs -original price)
+        public bool IsUsedInDiscount { get; set; }
+        public string Size { get; set; }
+
+        public Parcel(int parcelNumber,double maxDimension, double weight) {
+            ParcelNumber = parcelNumber;
+            MaxDimension = maxDimension;
+            Weight = weight;
+
+            GetParcelSize();
+            GetParcelDeliveryCost();
+        }
+        
+        public void GetParcelSize()
+        {
+            // should use switch 
+            if (MaxDimension < 10)
+            {
+                //use constant of enum
+                Size = "S";
+                DeliveryCost = 3;
+            }
+            else if (MaxDimension < 50)
+            {
+                Size = "M";
+                DeliveryCost = 8;
+            }
+            else if (MaxDimension < 100)
+            {
+                Size = "L";
+                DeliveryCost = 15;
+            }
+            else if (MaxDimension >= 100)
+            {
+                Size = "XL";
+                DeliveryCost = 25;
+            }
+        }
+        public void GetParcelDeliveryCost()
+        {
+            var weight = new Dictionary<string, double>();
+            //use constant of enum
+            weight.Add("S", 1);
+            weight.Add("M", 3);
+            weight.Add("L", 6);
+            weight.Add("XL", 10);
+            
+            //over weight
+            if (Weight > 50)
+            {
+                DeliveryCost = 50 + Weight - 50;
+            }
+            else if (Weight >  weight[Size])
+            {
+                var cost = DeliveryCost;
+                //need to round to closest
+                DeliveryCost = DeliveryCost + 2 * (int)(Weight - weight[Size]);
+            }
+        }
+    }
+}
